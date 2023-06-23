9ef647fe993c03ae2d36a8265da1a25d16df5b0a
Xion <xionwin@hotmail.com>
2023-06-23 20:14:29 +0800

    Update

--- Extension/SemanticExtension.cs
+++ Extension/SemanticExtension.cs
@@ -7,23 +7,4 @@ public static class SemanticExtension
         return source;
     }
 
-    public static TResult Then<T, TResult>(this T source, Func<T, TResult> function) =>
-    function.Invoke(source);
-
-    public static IEnumerator<T> Loop<T>(this IEnumerator<T> source, Func<T, bool> condition, Action<T> processing) =>
-    condition(source.Current) ? source.With(x => processing(x.Current)).With(x => x.MoveNext()).Loop(condition, processing) : source;
-
-    public static void Foreach<T>(this IEnumerable<T> items, Action<T, T?> handle) where T : struct
-    {
-        var current = default(T);
-        T? pervious = null;
-
-        foreach (var item in items)
-        {
-            current = item;
-            handle?.Invoke(current, pervious);
-            pervious = item;
-        }
-    }
-
 }
